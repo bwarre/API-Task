5cafa41f2ba9218f18f0bf5cb53cf9661d13b004
Benjamin Warren <bwarren@spartaglobal.com>
2022-07-22 13:50:28 +0100

    Adding in a short note. I beleive adding .json in the right places to convert to a dictionary will result in the code working.

--- single_season.py
+++ single_season.py
@@ -10,7 +10,14 @@ class SingleSeason():
         self.request = requests.get(self.url)
 
         s = ''.join(map(chr, self.request.content))  #converting from a method into a string
-        dict = json.loads(s)    #dict should be the dictionary
+        dict = json.loads(s)    #dict should be the dictionary, converting from string to json string.
+
+# json string does not behave like a dictionary. Data type is different, need to query
+
+
+
+        # JSON.LOADS IS NOT A DICTIONARY WE NEED TO USE .JSON TO CONVERT TO DICTIONARY!!!!!!!!!!!!!
+
 
         self.resp = dict   # converting from a string into a dictionary.
 
